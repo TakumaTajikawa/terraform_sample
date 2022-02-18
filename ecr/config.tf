565546db26d4154fa33970b5051db48f03944352
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-02-18 10:06:14 +0900

    46ページまで終了

---
+++ ecr/config.tf
@@ -0,0 +1,11 @@
+terraform { 
+    backend "s3" {
+        bucket = "terraform-3211-2051-3547"
+        key = "terraform-sample/ecr/.terraform/terraform.tfstate" 
+        region = "ap-northeast-1"
+    } 
+} 
+
+provider "aws" { 
+    region = "ap-northeast-1" 
+}
