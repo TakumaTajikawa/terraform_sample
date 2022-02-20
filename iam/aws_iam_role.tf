0407fdfa77c33e4fadacc59130019919a2e59840
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-02-20 19:19:55 +0900

    軽微な修正

--- iam/aws_iam_role.tf
+++ iam/aws_iam_role.tf
@@ -7,5 +7,5 @@ resource "aws_iam_role" "ecs_instance_role" {
 resource "aws_iam_role" "ecs_task_role" {
     name = "ecs-task-role"
     path = "/"
-    assume_role_policy = file("aws_iam_role_policies/ecs_task_assume_role_policy. json")
+    assume_role_policy = file("aws_iam_role_policies/ecs_task_assume_role_policy.json")
 }
