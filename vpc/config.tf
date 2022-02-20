bf06f20885d79222bae59c46b468cf6e191e2780
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-02-20 12:11:30 +0900

    VPCの設定ファイルを作成

---
+++ vpc/config.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "s3" { 
+        bucket = "terraform-3211-2051-3547"
+        key = "terraform-sample/vpc/terraform.tfstate"
+        region = "ap-northeast-1" 
+    } 
+} 
+
+provider "aws" {
+    region = "ap-northeast-1" 
+}
