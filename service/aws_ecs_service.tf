206d0c3b7a943f85440610735daee50a3c4770a4
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-02-21 09:35:21 +0900

    ECS関連のリソースを作成

---
+++ service/aws_ecs_service.tf
@@ -0,0 +1,16 @@
+resource "aws_ecs_service" "webapp-service" {
+    # ECSサービスの名前
+    name = "webapp-service"
+
+    # 作成したECSクラスタのIDを指定
+    cluster = aws_ecs_cluster.sample-ecs-cluster.id
+
+    # TaskDefinitionのarnを指定
+    task_definition = aws_ecs_task_definition.sample-task.arn
+
+    # TaskDefinition を実行するインスタンスの数を定義
+    desired_count = 1
+
+    # サービスを実行するタイプを EC2 か FARGATE を指定
+    launch_type = "EC2"
+}
