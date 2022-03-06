0052a3e9a13517b892e07deba53bc6c439647c60
Takuma Tajikawa <tajikawa@arsaga.jp>
2022-03-06 14:45:06 +0900

    RDSの設定に関する記述を追加

--- service/aws_security_group.tf
+++ service/aws_security_group.tf
@@ -1,11 +1,23 @@
 resource "aws_security_group" "instance" {
+
     # セキュリティグループの名前
     name = "instance"
+
     # セキュリティグループの説明
     description = "instance sg"
+
     # セキュリティグループリソースを作成するVPCを指定している
     vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
     
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        security_groups = [
+            aws_security_group.alb.id,
+        ]
+    }
+
     # セキュリティグループから外に出る通信のポート範囲の対象IPを指定している
     egress {
         from_port = 0
@@ -32,5 +44,21 @@ resource "aws_security_group" "alb" {
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "db" {
+    name = "sample-db"
+    description = "DB"
+    vpc_id = data.terraform_remote_state.vpc.vpc_id 
+    
+    ingress { 
+        from_port = 3306
+        to_port = 3306
+        protocol = "tcp"
+
+        security_groups = [
+            aws_security_group.instance.id,
+        ]
     }
 }
